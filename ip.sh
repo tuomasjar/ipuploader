2219d917af4ee162de1a338484faa1d3ca79cdf5
Tuomas Järvinen <31968679+tuomasjar@users.noreply.github.com>
2020-10-17 19:18:13 +0300

    Update ip.sh
    added comments

--- ip.sh
+++ ip.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 #echo "Starting"
-T1=$(curl ifconfig.me)
+T1=$(curl ifconfig.me) #Get external ip
 #echo $T1
-T2=$(tail -1 ip.txt)
+T2=$(tail -1 ip.txt) #Read the latest ip in the file
 #echo $T2
-if [ !  "$T1" = "$T2" ]; then
+if [ !  "$T1" = "$T2" ]; then #if different update and upload
         echo "\n" >> ip.txt
         echo $T1 >> ip.txt
         cd /home/pi/Dropbox-Uploader/
         ./dropbox_uploader.sh upload /home/pi/Scripts/ip.txt ip.txt
 else
-        echo "Found"
+        echo "Found" #already is the latest in the file
 fi
