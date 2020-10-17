2f8b635444b475d4233394e1b298d3d093d93572
Tuomas Järvinen <31968679+tuomasjar@users.noreply.github.com>
2020-10-17 21:00:54 +0300

    Update ip.sh
    Added timestamp and removed excessive new line

--- ip.sh
+++ ip.sh
@@ -5,10 +5,10 @@ T1=$(curl ifconfig.me) #Get external ip
 T2=$(tail -1 ip.txt) #Read the latest ip in the file
 #echo $T2
 if [ !  "$T1" = "$T2" ]; then #if different update and upload
-        echo "\n" >> ip.txt
+        echo &date >> ip.txt
         echo $T1 >> ip.txt
-        cd /home/pi/Dropbox-Uploader/
-        ./dropbox_uploader.sh upload /home/pi/Scripts/ip.txt ip.txt
+        cd /path/to/Dropbox-Uploader/
+        ./dropbox_uploader.sh upload /path/to/Scripts/ip.txt ip.txt
 else
         echo "Found" #already is the latest in the file
 fi
