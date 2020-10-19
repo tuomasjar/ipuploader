064d9c1a78e685eba4627432f000f8d89164f799
Tuomas Järvinen <31968679+tuomasjar@users.noreply.github.com>
2020-10-19 09:23:26 +0300

    Update ip.sh
    checking if it is too long and then just adding "Server error" to log (had one with lenghty error from server)

--- ip.sh
+++ ip.sh
@@ -1,15 +1,24 @@
 #!/bin/sh
 #echo "Starting"
-cd /path/to/Script/
-T1=$(curl ifconfig.me) #Get external ip
+cd /home/pi/Scripts/
+T1=$(curl ifconfig.me)
 #echo $T1
-T2=$(tail -1 ip.txt) #Read the latest ip in the file
+T2=$(tail -1 ip.txt)
 #echo $T2
-if [ !  "$T1" = "$T2" ]; then #if different update and upload
-        echo &date >> ip.txt
-        echo $T1 >> ip.txt
-        cd /path/to/Dropbox-Uploader/
-        ./dropbox_uploader.sh upload /path/to/Scripts/ip.txt ip.txt
+#echo ${#T1}
+if [ ${#T1} -le 20 ] ; then
+        if [ !  "$T1" = "$T2" ]; then
+                echo &date >> ip.txt
+                echo $T1 >> ip.txt
+                cd /home/pi/Dropbox-Uploader/
+                ./dropbox_uploader.sh upload /home/pi/Scripts/ip.txt ip.txt
+        else
+                echo "Found"
+        fi
 else
-        echo "Found" #already is the latest in the file
+        echo &date >> ip.txt
+        echo "Server error" >> ip.txt
+        cd /home/pi/Dropbox-Uploader/
+        ./dropbox_uploader.sh upload /home/pi/Scripts/ip.txt ip.txt
 fi
+
